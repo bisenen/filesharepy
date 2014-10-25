fc1a28a3651ccf6c94dc1be7f46331bd088dee2b
bisenen <magaster@gmail.com>
2014-10-26 03:25:18 +0500

    add convert image

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from werkzeug import secure_filename
 from hashids import Hashids
 import dbexe
 import random
-
+import Image
 
 app = Flask(__name__)
 
@@ -25,6 +25,14 @@ print hashids.encrypt(random.randint(100000000, 9000000000))
 
 int_db = dbexe.MainDb(app.config['DB_NAME'])
 
+def make_pre_image(path):
+    basewidth = 300
+    name = "pre_{0}".format(os.path.basename(path))
+    img = Image.open(path)
+    wpercent = (basewidth/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+    img.save(os.path.join(full_path_uploads, name))
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -44,8 +52,10 @@ def upload():
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = "{0}.{1}".format(hashids.encrypt(random.randint(1000000000000000000, 9000000000000000000)), file.filename.rsplit('.',1)[1])
-            file.save(os.path.join(app.root_path, app.config['UPLOADER_FOLDER'], filename))
-            int_db.insert_files(filename, os.path.join(app.root_path, app.config['UPLOADER_FOLDER'], filename), "/uploads/{0}".format(filename))
+            full_path_name = os.path.join(app.root_path, app.config['UPLOADER_FOLDER'], filename)
+            file.save(full_path_name)
+            int_db.insert_files(filename, full_path_name, "/uploads/{0}".format(filename))
+            make_pre_image(full_path_name)
             return redirect(url_for('uploaded_file', filename=filename))
 
 @app.route('/uploads/<filename>')
