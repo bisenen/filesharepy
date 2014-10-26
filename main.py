322576567edaf6f36e0dea37c9ffcbec493e1597
bisenen <magaster@gmail.com>
2014-10-27 00:45:15 +0300

    fix html galley

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-
 import os
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-from werkzeug import secure_filename
-from hashids import Hashids
-import dbexe
 import random
+
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from hashids import Hashids
 import Image
+
+import dbexe
+
 
 app = Flask(__name__)
 
@@ -29,36 +30,33 @@ print int_db.read_db('name')
 
 def make_pre_image(path):
     max_size = 200
-    print path
     name = "pre_{0}".format(os.path.basename(path))
     img = Image.open(path)
     if img.size[0] > img.size[1]:
-        wpercent = (max_size/float(img.size[0]))
-        hsize = int((float(img.size[1])*float(wpercent)))
-        print hsize
-        print 'A'
+        wpercent = (max_size / float(img.size[0]))
+        hsize = int((float(img.size[1]) * float(wpercent)))
         img = img.resize((max_size, hsize), Image.ANTIALIAS)
     else:
-        hpercent = (max_size/float(img.size[1]))
-        wsize = int((float(img.size[0])*float(hpercent)))
-        print wsize
-        print "B"
+        hpercent = (max_size / float(img.size[1]))
+        wsize = int((float(img.size[0]) * float(hpercent)))
         img = img.resize((wsize, max_size), Image.ANTIALIAS)
-    print img
-    print type(img)
     img.save(os.path.join(full_path_uploads, name))
+
 
 def allowed_file(filename):
     return '.' in filename and \
-        filename.rsplit('.',1)[1] in app.config['ALLOWED_EXTENSIONS']
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
 
 def check_upload_folder():
     if not os.path.isdir(full_path_uploads):
         os.mkdir(full_path_uploads)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/list')
 def gallery():
@@ -67,14 +65,16 @@ def gallery():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = "{0}.{1}".format(hashids.encrypt(random.randint(1000000000000000000, 9000000000000000000)), file.filename.rsplit('.',1)[1])
-            full_path_name = os.path.join(app.root_path, app.config['UPLOADER_FOLDER'], filename)
-            file.save(full_path_name)
-            int_db.insert_files(filename, full_path_name, "/uploads/{0}".format(filename))
-            make_pre_image(full_path_name)
-            return redirect(url_for('uploaded_file', filename=filename))
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = "{0}.{1}".format(hashids.encrypt(random.randint(1000000000000000000, 9000000000000000000)),
+                                    file.filename.rsplit('.', 1)[1])
+        full_path_name = os.path.join(app.root_path, app.config['UPLOADER_FOLDER'], filename)
+        file.save(full_path_name)
+        int_db.insert_files(filename, full_path_name, "/uploads/{0}".format(filename))
+        make_pre_image(full_path_name)
+        return redirect(url_for('uploaded_file', filename=filename))
+
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
